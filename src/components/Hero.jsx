143cbe2cfa0b7b551d96748550092ca1400df128
PiyuminiWelikala <38849112+PiyuminiWelikala@users.noreply.github.com>
2020-07-23 21:20:49 +0530

    Initial React Project Setup

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
 
 class Hero extends Component {
-  state = { heroId: 189 };
+  state = {
+    heroId: 189,
+    movies: ["Movie 1", "Movie 2", "Movie 3"],
+    likeCount: 0
+  };
   render() {
     return (
-      <React.Fragment>
-        <h1>Avengers incoming ...</h1>
-        <button>Click Here {this.state.heroId} </button>
-      </React.Fragment>
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://p1.hiclipart.com/preview/707/869/118/the-a-avengers-logo-png-clipart.jpg"
+          className="card-img-top" alt="No image"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Avengers Name</h5>
+          <h6>Avenger Birth Name</h6>
+          <ul>{this.showMovies()}</ul>
+          <button className="btn btn-info" onClick={() => {this.likeAvenger(1)}}>
+            Like{" "}
+    <span className="badge badge-light">{this.state.likeCount}</span>
+          </button>
+        </div>
+      </div>
     );
   }
+  isHero() {
+    //if (this.state.heroId < 0) return "Not an avenger";
+    //else return "Is an avenger";
+
+    return this.state.heroId < 0 ? "Not an avenger" : "Is an avenger";
+  }
+  showMovies() {
+    if (this.state.movies.length === 0) return <p>No movie available</p>;
+    return this.state.movies.map((movie) => <li key={this.state.movies.indexOf(movie)}>{movie}</li>);
+  }
+  likeAvenger = (likeCounter) => {
+    //console.log("You have liked this Avenger!");
+    this.setState({likeCount : this.state.likeCount + likeCounter});
+  };
 }
 
 export default Hero;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css"
-import Hero from './components/Hero';
+//import Hero from './components/Hero';
+import Heroes from './components/Heroes';
 
-ReactDOM.render(<Hero/> , document.getElementById('root'));
+ReactDOM.render(<Heroes/> , document.getElementById('root'));
